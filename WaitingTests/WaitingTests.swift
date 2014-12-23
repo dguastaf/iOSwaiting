b78da4a0758784426542261be25b4cc388d78f25
Doug Guastaferro <dguastaferro@linkedin.com>
2014-12-22 20:27:15 -0800

    Added parse files

---
+++ WaitingTests/WaitingTests.swift
@@ -0,0 +1,36 @@
+//
+//  WaitingTests.swift
+//  WaitingTests
+//
+//  Created by Doug Guastaferro on 12/22/14.
+//  Copyright (c) 2014 Doug Guastaferro. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WaitingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
